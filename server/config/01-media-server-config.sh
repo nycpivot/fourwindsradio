801b7ed7e4667263d3cd1365ef85b331c1b21e5a
mjames <developer@thejameshome.net>
2024-02-04 21:01:45 -0500

    Install RTMP nginx and ffmpeg.

--- server/config/01-media-server-config.sh
+++ server/config/01-media-server-config.sh
@@ -1,28 +1,29 @@
-STEP 1
+#!/bin/bash
+
 sudo apt update
 sudo apt install libnginx-mod-rtmp
-sudo nano /etc/nginx/nginx.conf
-WRITE IN
-rtmp {
-        server {
-                listen 1935;
-                chunk_size 4096;
-                allow publish 127.0.0.1;
-                deny publish all;
 
-                application live {
-                        live on;
-                        record off;
-                }
-        }
+cat <<EOF | tee rtmp-config.json
+
+rtmp {
+  server {
+    listen 1935;
+    chunk_size 4096;
+    allow publish 127.0.0.1;
+    deny publish all;
+    application live {
+      live on;
+      record off;
+    }
+  }
 }
+EOF
+
+cat rtmp-config.json >> /etc/nginx/nginx.conf
 
 sudo ufw allow 1935/tcp
 sudo systemctl reload nginx.service
 
-STEP 2
-sudo apt install python3-pip
-sudo pip install youtube-dl
-youtube-dl address -f mp4
 sudo apt install ffmpeg
-ffmpeg -re -i "VIDEO NAME" -c:v copy -c:a aac -ar 44100 -ac 1 -f flv rtmp://localhost/live/stream
+
+#ffmpeg -re -i "VIDEO NAME" -c:v copy -c:a aac -ar 44100 -ac 1 -f flv rtmp://localhost/live/stream
